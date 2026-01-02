ed49081357656998535b85e11c978041c4cc3011
Mordenkian <jasonwang7041@gmail.com>
2026-01-02 14:41:52 -0800

    Fixed Naming error

--- weatherapp/lib/dbConnect.ts
+++ weatherapp/lib/dbConnect.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URL = process.env.MONGODB_URL;
 
-if (!MONGODB_URI) {
+if (!MONGODB_URL) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
+    "Please define the MONGODB_URL environment variable inside .env.local"
   );
 }
 
@@ -24,7 +24,7 @@ async function dbConnect() {
     return cached.conn;
   }
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URI!);
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URL!);
   cached.conn = await cached.promise;
   return cached.conn;
 }
